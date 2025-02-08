d912522f34be9e495149a0124e996d768605c714
RuskaGo4ort <132620477+RuskaGo4ort@users.noreply.github.com>
2025-02-08 13:12:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ const button1 = document.getElementById("btn1");
 const button2 = document.getElementById("btn2");
 const img = document.getElementById("img1");
 const h1 = document.getElementById("h1");
-
+const audio = document.getElementById("sound");
 
 function Ahuela() {
-    button1.style.width = "2000px";
-    button1.style.height = "2000px";
+    button1.style.width = "100%";
+    button1.style.height = "100%";
     button1.style.cursor = "pointer";
     button1.style.color = "#ffffff"
     button1.style.backgroundColor = "#E01010FF"
     button1.style.fontWeight = "bold"
     button1.textContent = 'Я СКАЗАЛ "ДА"'
     button1.style.fontSize = "100px";
-    button2.remove()
+    button2.remove();
+    audio.play(); // Добавляем звук при клике
 }
+
 function NeAhuela() {
     h1.textContent = "УРАААА";
     h1.style.color = "#09dc09"
     img.src = "Agu.gif";
-    button1.remove()
-    button2.remove()
-
+    button1.remove();
+    button2.remove();
+    audio.play();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    audio.volume = 0.3;
+});
